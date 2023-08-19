3f17ee2e9fa3e5e17532376dc51c509757582c4b
Pranjal Pandey <pranjaltech10@gmail.com>
2023-08-19 14:54:35 +0530

    add more comments

--- digits.py
+++ digits.py
@@ -1,3 +1,5 @@
+# This example shows how scikit-learn can be used to recognize images of hand-written digits, from 0-9.
+
 # Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
 # License: BSD 3 clause
 
@@ -8,6 +10,10 @@ import matplotlib.pyplot as plt
 from sklearn import datasets, metrics, svm
 from sklearn.model_selection import train_test_split
 
+# The digits dataset consists of 8x8 pixel images of digits. The images attribute of the dataset stores 8x8 arrays of grayscale values for each image. We will use these arrays to visualize the first 4 images. The target attribute of the dataset stores the digit each image represents and this is included in the title of the 4 plots below.
+
+# Note: if we were working from image files (e.g., ‘png’ files), we would load them using matplotlib.pyplot.imread.
+
 # 1. Data Loading
 digits = datasets.load_digits()
 
@@ -17,6 +23,10 @@ for ax, image, label in zip(axes, digits.images, digits.target):
     ax.set_axis_off()
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
     ax.set_title("Training: %i" % label)
+
+# To apply a classifier on this data, we need to flatten the images, turning each 2-D array of grayscale values from shape (8, 8) into shape (64,). Subsequently, the entire dataset will be of shape (n_samples, n_features), where n_samples is the number of images and n_features is the total number of pixels in each image.
+
+# We can then split the data into train and test subsets and fit a support vector classifier on the train samples. The fitted classifier can subsequently be used to predict the value of the digit for the samples in the test subset.
 
 # flatten the images
 # 3. Data pre processing
@@ -40,6 +50,9 @@ clf.fit(X_train, y_train)
 # 6. Model Predictino
 predicted = clf.predict(X_test)
 
+# Below we visualize the first 4 test samples and show their predicted digit value in the title.
+
+
 # 7. Quantitative sanity check 
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
 for ax, image, prediction in zip(axes, X_test, predicted):
@@ -48,11 +61,15 @@ for ax, image, prediction in zip(axes, X_test, predicted):
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
     ax.set_title(f"Prediction: {prediction}")
 
+# classification_report builds a text report showing the main classification metrics.
+
 #8. Model Evaluation
 print(
     f"Classification report for classifier {clf}:\n"
     f"{metrics.classification_report(y_test, predicted)}\n"
 )
+
+# We can also plot a confusion matrix of the true digit values and the predicted digit values.
 
 disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
 disp.figure_.suptitle("Confusion Matrix")
