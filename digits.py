fb84e6cecc764fcb1a64fbd312f53ebe0d08a52b
Pranjal Pandey <pranjaltech10@gmail.com>
2023-08-19 14:50:42 +0530

    add comments

--- digits.py
+++ digits.py
@@ -8,8 +8,10 @@ import matplotlib.pyplot as plt
 from sklearn import datasets, metrics, svm
 from sklearn.model_selection import train_test_split
 
+# 1. Data Loading
 digits = datasets.load_digits()
 
+# 2. Data Visualization
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
 for ax, image, label in zip(axes, digits.images, digits.target):
     ax.set_axis_off()
@@ -17,6 +19,7 @@ for ax, image, label in zip(axes, digits.images, digits.target):
     ax.set_title("Training: %i" % label)
 
 # flatten the images
+# 3. Data pre processing
 n_samples = len(digits.images)
 data = digits.images.reshape((n_samples, -1))
 
@@ -24,16 +27,20 @@ data = digits.images.reshape((n_samples, -1))
 clf = svm.SVC(gamma=0.001)
 
 # Split data into 50% train and 50% test subsets
+# 4. Data splitting
 X_train, X_test, y_train, y_test = train_test_split(
     data, digits.target, test_size=0.5, shuffle=False
 )
 
 # Learn the digits on the train subset
+# 5. Model Training
 clf.fit(X_train, y_train)
 
 # Predict the value of the digit on the test subset
+# 6. Model Predictino
 predicted = clf.predict(X_test)
 
+# 7. Quantitative sanity check 
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
 for ax, image, prediction in zip(axes, X_test, predicted):
     ax.set_axis_off()
@@ -41,6 +48,7 @@ for ax, image, prediction in zip(axes, X_test, predicted):
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
     ax.set_title(f"Prediction: {prediction}")
 
+#8. Model Evaluation
 print(
     f"Classification report for classifier {clf}:\n"
     f"{metrics.classification_report(y_test, predicted)}\n"
