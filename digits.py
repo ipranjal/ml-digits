0d6245634902ed2bf9f4f838ced6e9fc504d8731
Pranjal Pandey <pranjaltech10@gmail.com>
2023-08-26 14:13:57 +0530

    test-train splitting fn

--- digits.py
+++ digits.py
@@ -21,7 +21,13 @@ def preprocess_data(data):
     data = digits.images.reshape((n_samples, -1))
     return data
 
+# Split data into 50% train and 50% test subsets
 
+def split_data(X,y,test_size=0.5,random_state=1):
+    X_train, X_test, y_train, y_test = train_test_split(
+     X, y, test_size=test_size, shuffle=False
+    )
+    return X_train, X_test, y_train, y_test
 
 # 1. Data Loading
 digits = datasets.load_digits()
@@ -33,21 +39,16 @@ for ax, image, label in zip(axes, digits.images, digits.target):
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
     ax.set_title("Training: %i" % label)
 
-# To apply a classifier on this data, we need to flatten the images, turning each 2-D array of grayscale values from shape (8, 8) into shape (64,). Subsequently, the entire dataset will be of shape (n_samples, n_features), where n_samples is the number of images and n_features is the total number of pixels in each image.
-
-# We can then split the data into train and test subsets and fit a support vector classifier on the train samples. The fitted classifier can subsequently be used to predict the value of the digit for the samples in the test subset.
 
 # 3. Data Preprocessing
 data = preprocess_data(digits.data)
 
+# 4. Data splitting
+X_train, X_test, y_train, y_test = train_test_split(data, digits.target, test_size=0.3);
+
 # Create a classifier: a support vector classifier
 clf = svm.SVC(gamma=0.001)
 
-# Split data into 50% train and 50% test subsets
-# 4. Data splitting
-X_train, X_test, y_train, y_test = train_test_split(
-    data, digits.target, test_size=0.5, shuffle=False
-)
 
 # Learn the digits on the train subset
 # 5. Model Training
