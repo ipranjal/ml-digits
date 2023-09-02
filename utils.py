63c1585b02cbc2ca0184b26bc7b60103d1d591a0
Pranjal Pandey <pranjaltech10@gmail.com>
2023-09-02 13:42:56 +0530

    show predictions

--- utils.py
+++ utils.py
@@ -39,11 +39,22 @@ def split_train_dev_test(X, y, test_size, dev_size):
     return X_train, X_dev, X_test, y_train, y_dev, y_test
 
 def predict_and_eval(model, X_test, y_test):
+    
     predicted = model.predict(X_test)
+
+    _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+    for ax, image, prediction in zip(axes, X_test, predicted):
+        ax.set_axis_off()
+        image = image.reshape(8, 8)
+        ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+        ax.set_title(f"Prediction: {prediction}")
+
+
     print(
     f"Classification report for classifier {model}:\n"
     f"{metrics.classification_report(y_test, predicted)}\n"
     )
+    
     disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
     disp.figure_.suptitle("Confusion Matrix")
     print(f"Confusion matrix:\n{disp.confusion_matrix}")
