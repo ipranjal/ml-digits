be0ad3c8656717b832c470fe799d83a456a39b81
Pranjal Pandey <pranjaltech10@gmail.com>
2023-11-07 21:46:39 +0530

    fix model path

--- api/app.py
+++ api/app.py
@@ -13,10 +13,10 @@ def pred_model():
     image1 = js['image1']
     image2 = js['image2']
     #Assuming this is the path of our best trained model
-    model = load('./models/svm_gamma:0.001_C:0.1.joblib')
+    model = load('/Users/pranjal/Desktop/ml-ops/models/svmgamma:0.001_C:1.joblib')
     prediction_image_1 = model.predict(image1)
     prediction_image_2 = model.predict(image2)
     if(prediction_image_1 == prediction_image_2):
-        return True
+        return "True"
     else:
-        return False
+        return "False"
