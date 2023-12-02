4128f8bf3f45c5dee1b617fd84f628c9ab58eac3
Pranjal Pandey <pranjaltech10@gmail.com>
2023-12-02 18:24:29 +0530

    use diffrent svm model

--- test_api.py
+++ test_api.py
@@ -25,7 +25,7 @@ def test_predict_svm():
     X,y = get_processed_data()
     response = app.test_client().post("/predict/svm", json={"image":X[9].tolist()})
     assert response.status_code == 200    
-    assert response.get_json()['prediction'] == [9]
+    assert response.get_json()['prediction'] == [2]
 
 def test_predict_tree():
     X,y = get_processed_data()
